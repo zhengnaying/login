b99a94c469c40a65cf57792f42a913e9206a9f58
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-11-09 18:13:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.login;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt = (Button)findViewById(R.id.b1);
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final View viewDialog = LayoutInflater.from(MainActivity.this).inflate(R.layout.customdialog,null,false);
+        builder.setTitle("登录对话框")
+                .setView(viewDialog)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText User_id = viewDialog.findViewById(R.id.editText_User_id);
+                        EditText User_pw = viewDialog.findViewById(R.id.editText_User_pw);
+                        String id = User_id.getText().toString();
+                        String pw = User_pw.getText().toString();
+                        if(id.equals("abc")  && pw.equals("123"))
+                            Toast.makeText(MainActivity.this,"登录成功！",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"登录失败!",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+        builder.create().show();
+
+    }
+}
